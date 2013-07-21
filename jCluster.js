32e95356bd4928588dcb03fbe3570e8a94858178
Dadi <zhaodadi@gmail.com>
2013-07-21 23:41:41 +1000

    initial

---
+++ jCluster.js
@@ -0,0 +1,15 @@
+(function( $ ) {
+ 
+    $.fn.jCluster = function( action ) {
+ 
+        if ( action === "open") {
+            // Open popup code.
+        }
+ 
+        if ( action === "close" ) {
+            // Close popup code.
+        }
+ 
+    };
+ 
+}( jQuery ));
